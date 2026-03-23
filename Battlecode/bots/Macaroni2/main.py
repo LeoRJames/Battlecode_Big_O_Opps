3b066339587601d604011fb891044031b1ec5608
Aarnav Walia <aarnavwalia@gmail.com>
2026-03-23 12:32:37 +0000

    Cleaned up function names + fixed harvester bugs

--- Battlecode/bots/Macaroni2/main.py
+++ Battlecode/bots/Macaroni2/main.py
@@ -15,31 +15,34 @@ class Player:
         self.enemy_core_position = Position(1000, 1000)
         self.target = Position(0, 0)
         self.status = 0
-        self.last_positions = [Position(0, 0), Position(0,0)]
+        self.last_positions = []
 
 
-    def initialise(self, ct):
+    def bb_initialise(self, ct):
         vision_tiles = ct.get_nearby_tiles()
         for i in vision_tiles:
             if ct.get_entity_type(ct.get_tile_building_id(i)) == EntityType.MARKER and ct.get_team(ct.get_tile_building_id(i)) == ct.get_team():
                 marker_value = ct.get_marker_value(ct.get_tile_building_id(i))
                 marker_value_id = (marker_value % (2 ** 28)) // (2 ** 12)
                 marker_status = marker_value // (2 ** 28)
-                ct.draw_indicator_dot(ct.get_position(), 0, 0, 0)
 
                 if marker_value_id == ct.get_id(): # if marker is referring to this bot
+
+                    # Destroy marker if it can
                     if ct.can_move(ct.get_position().direction_to(i)):
                         ct.move(ct.get_position().direction_to(i))
                     if ct.can_destroy(i):   # Destroy marker
                         ct.destroy(i)
+
                     if marker_status == 1:
                         # Load Position to check for opponent core
                         target_x = (marker_value % (2 ** 12)) // (2 ** 6)
                         target_y = marker_value % (2 ** 6)
                         self.target = Position(target_x, target_y)
                         self.status = 1
+
                     if marker_status == 2:
-                        # Load Position to check for opponent core
+                        # Load Position to go attack opponent core
                         target_x = (marker_value % (2 ** 12)) // (2 ** 6)
                         target_y = marker_value % (2 ** 6)
                         self.target = Position(target_x, target_y)
@@ -50,8 +53,6 @@ class Player:
             # Save position of the core
             if ct.get_entity_type(ct.get_tile_building_id(ct.get_position())) == EntityType.CORE and ct.get_team(ct.get_tile_building_id(ct.get_position())) == ct.get_team():
                 self.core_pos = ct.get_position(ct.get_tile_building_id(ct.get_position()))
-            else:
-                ct.draw_indicator_dot(ct.get_position(), 0, 0, 0)
 
             # Where road should be - Currently Core does not try to place a marker down a second time, if it can not the first time.
             if ct.get_entity_type(ct.get_tile_building_id(self.core_pos.add(Direction.NORTHWEST).add(Direction.NORTH))) == EntityType.ROAD:
@@ -59,18 +60,26 @@ class Player:
                     ct.destroy(self.core_pos.add(Direction.NORTHWEST).add(Direction.NORTH))
 
 
-    def find_enemy_core(self, ct):
+    def bb_find_enemy_core(self, ct):
         pos = ct.get_position()
 
-        self.go_to(ct)
+        self.bb_go_to(ct)
 
         if ct.get_current_round() > 100: # Dont get stuck
             self.status = 3
+            self.last_positions = []
             return
 
         if ct.is_in_vision(self.target):
+            self.last_positions = []
             building_id = ct.get_tile_building_id(self.target)
             if building_id and ct.get_entity_type(building_id) == EntityType.CORE: # ENEMY CORE HAS BEEN FOUND!
+
+                vision_tiles = ct.get_nearby_tiles()
+                for i in vision_tiles:
+                    idd = ct.get_tile_building_id(i)
+                    if idd and ct.get_entity_type(idd) == EntityType.CORE:
+                        ct.draw_indicator_dot(i, 255, 255, 255)
                 self.target = self.core_pos
                 enemy_core_x, enemy_core_y = ct.get_position(building_id).x, ct.get_position(building_id).y
                 self.enemy_core_position = ct.get_position(building_id)
@@ -90,10 +99,9 @@ class Player:
 
             # Core was not at the location (could change to inform home about this?)
             self.status = 3
-            ct.draw_indicator_dot(pos, 0, 200, 200)
 
 
-    def spread_the_news_about_said_enemy_core(self, ct):
+    def bb_spread_the_news_about_said_enemy_core(self, ct):
 
         pos = ct.get_position()
         enemy_core_x, enemy_core_y = self.enemy_core_position.x, self.enemy_core_position.y
@@ -113,7 +121,7 @@ class Player:
                     return
             if ct.can_place_marker(pos.add(i)):
                 ct.place_marker(pos.add(i), message)
-                self.go_to(ct)
+                self.bb_go_to(ct)
                 return
 
         if ct.get_position().distance_squared(self.core_pos) <= 9:
@@ -121,10 +129,10 @@ class Player:
             for i in vision_tiles:
                 if ct.is_tile_empty(i):
                     self.target=i
-        self.go_to(ct)
+        self.bb_go_to(ct)
 
 
-    def find_ores(self, ct):
+    def bb_find_ores(self, ct):
         vision_tiles = ct.get_nearby_tiles()
         adj_tiles = ct.get_nearby_tiles(4)
         pos = ct.get_position()
@@ -139,6 +147,7 @@ class Player:
                             ct.move(d)
                 if ct.can_build_harvester(j):
                     ct.build_harvester(j)
+                    self.last_positions = []
                     return
 
                 self.target = self.core_pos
@@ -155,24 +164,27 @@ class Player:
                             dir_A = i.direction_to(self.target)
                             dir_B = i.add(dir_A).direction_to(self.target)
                             dir_C = i.add(dir_A).add(dir_B).direction_to(self.target)
-                            goals = [i.add(d).add(dir_A).add(dir_B).add(dir_C), i.add(d).add(dir_A).add(dir_B)] + [i.add(d).add(dir_A).add(dir_B).add(k) for k in DIRECTIONS] + [i.add(d).add(dir_A).add(k)for k in DIRECTIONS]
+
+                            goals = [i.add(d).add(dir_A).add(dir_B).add(dir_C)] + [i.add(d).add(dir_A).add(dir_B).add(dir_C).add(k) for k in DIRECTIONS] + [i.add(d).add(dir_A).add(dir_B).add(k) for k in DIRECTIONS] + [i.add(d).add(dir_A).add(k).add(k) for k in DIRECTIONS]
                             for k in range(len(goals)):
-                                ct.draw_indicator_line(i.add(d), goals[k], 50 * k, 50 * k, 50 * k)
                                 if (ct.can_build_bridge(i.add(d), goals[k])
-                                        and (ct.get_tile_building_id(goals[k]) is None or ct.get_team() == ct.get_team(
-                                            ct.get_tile_building_id(goals[k])))
-                                        and (ct.get_tile_env(goals[k]) == Environment.EMPTY)):
+                                        and ((ct.get_tile_building_id(goals[k]) is None) or ct.get_team() == ct.get_team(ct.get_tile_building_id(goals[k])))
+                                        and (ct.get_tile_env(goals[k]) == Environment.EMPTY)
+                                        and (ct.get_tile_env(i.add(d)) == Environment.EMPTY)):
                                     ct.draw_indicator_line(i.add(d), goals[k], 0, 255, 0)
                                     ct.build_bridge(i.add(d), goals[k])
+                                    self.last_positions = []
                                     self.target = goals[k]
                                     self.status = 5
                                     return
+                                elif ct.can_build_bridge(i.add(d), goals[k]):
+                                    ct.draw_indicator_line(i.add(d), goals[k], 250 , 0, 0)
 
         vision_tiles = ct.get_nearby_tiles()
         for i in vision_tiles:
             if ct.get_tile_env(i) in [Environment.ORE_TITANIUM, Environment.ORE_AXIONITE] and not(ct.get_entity_type(ct.get_tile_building_id(i))== EntityType.HARVESTER):
                 self.target = i
-                self.go_to(ct)
+                self.bb_go_to(ct)
                 return
 
         # Well and truly lost, so just keeping moving forward, if it can.
@@ -181,15 +193,17 @@ class Player:
         pos = ct.get_position()
         if ct.can_move(self.dir):
             ct.move(self.dir)
+            self.last_positions.append(pos)
         elif ct.can_build_road(pos.add(self.dir)):
             ct.build_road(pos.add(self.dir))
             if ct.can_move(self.dir):
                 ct.move(self.dir)
+                self.last_positions.append(pos)
         else:
             self.dir = random.choice(DIRECTIONS)
 
 
-    def build_conveyor_home(self, ct):
+    def bb_build_conveyor_home(self, ct):
         pos = ct.get_position()
         self.target = self.core_pos
 
@@ -201,10 +215,10 @@ class Player:
             dir_A = pos.direction_to(self.target)
             dir_B = pos.add(dir_A).direction_to(self.target)
             dir_C = pos.add(dir_A).add(dir_B).direction_to(self.target)
+            goals = [pos.add(dir_A).add(dir_B).add(dir_C)] + [pos.add(dir_A).add(dir_B).add(dir_C).add(k) for k in DIRECTIONS] + [pos.add(dir_A).add(dir_B).add(k) for k in DIRECTIONS] + [pos.add(dir_A).add(dir_B).add(k).add(k) for k in DIRECTIONS] + [pos.add(dir_A).add(dir_B).add(k).add(k.rotate_left()) for k in DIRECTIONS] + [pos.add(dir_A).add(dir_B).add(k).add(k.rotate_right()) for k in DIRECTIONS]
 
-            goals = [pos.add(dir_A).add(dir_B).add(dir_C), pos.add(dir_A).add(dir_B)] + [pos.add(dir_A).add(dir_B).add(i) for i in DIRECTIONS] + [pos.add(dir_A).add(i) for i in DIRECTIONS]
             for i in range(len(goals)):
-                ct.draw_indicator_line(pos, goals[i], 50*i, 50*i, 50*i)
+                ct.draw_indicator_line(pos, goals[i], 10*i, 10*i, 10*i)
 
                 if (ct.can_build_bridge(pos, goals[i])
                         and (ct.get_tile_building_id(goals[i]) is None or ct.get_team() == ct.get_team(ct.get_tile_building_id(goals[i])))
@@ -229,25 +243,32 @@ class Player:
             return
 
         else: # EDGE CASE
-            ct.draw_indicator_dot(pos, 255, 255, 255)
             self.status = 3
 
         if ct.get_position().distance_squared(self.core_pos) <= 4:
             self.status = 3
 
 
-    def go_to(self, ct, status=3, set_status=False):
+    def bb_go_to(self, ct, status=3, set_status=False):
+        '''
+            If there is e.g. enemy bot on target tile,
+        '''
         pos = ct.get_position()
         move_dir = pos.direction_to(self.target)
 
-        for i in range(8):
-            ct.draw_indicator_dot(pos.add(move_dir), 250, 250, 250)
-            if pos.add(move_dir) in self.last_positions:
-                move_dir = move_dir.rotate_left()
-                ct.draw_indicator_dot(pos, 250, 0, 0)
-                pass
+        if len(self.last_positions) > 20: # if stuck in a loop, show its ppath
+            for i in range(len(self.last_positions) -1):
+                ct.draw_indicator_line(self.last_positions[i], self.last_positions[i+1], 5*i, 50, 50)
 
-            if not (pos.add(move_dir).x >= 0 and pos.add(move_dir).y >= 0 and pos.add(move_dir).x <= ct.get_map_width() and pos.add(move_dir).y <= ct.get_map_height()):
+        for i in range(8):
+            if i == 7:
+                return False
+
+            if pos.add(move_dir) in self.last_positions and pos.add(move_dir) != self.target:
+                move_dir = move_dir.rotate_left()
+                continue
+
+            if not(pos.add(move_dir).x >= 0 and pos.add(move_dir).y >= 0 and pos.add(move_dir).x < ct.get_map_width() and pos.add(move_dir).y < ct.get_map_height()):
                 continue
             building_id = ct.get_tile_building_id(pos.add(move_dir))
             if not (ct.is_tile_passable(pos.add(move_dir))) and ct.can_destroy(
@@ -261,29 +282,31 @@ class Player:
                     break
             else:  # else move clockwise around the target
                 move_dir = move_dir.rotate_left()
-            if i == 7:
-                return False
-        self.last_positions[1] = self.last_positions[0]
-        self.last_positions[0] = pos
+
+        self.last_positions.append(pos)
         self.dir = move_dir
         ct.move(self.dir)
 
         if set_status and ct.get_position() == self.target:
+            self.last_positions = []
             self.status = status
 
 
-    def go_to_enemy_core(self, ct):
+    def bb_go_to_enemy_core(self, ct):
         pos = ct.get_position()
-        vision_tiles = ct.get_nearby_tiles()
+        self.bb_go_to(ct)
 
-        self.go_to(ct)
         if pos.distance_squared(self.enemy_core_position) <= 10:
+            vision_tiles = ct.get_nearby_tiles()
+            for i in vision_tiles:
+                if ct.get_entity_type(ct.get_tile_building_id(i)) == EntityType.CORE:
+                    ct.draw_indicator_dot(i, 0, 0, 0)
             self.status = 7
             self.target = Position(1000,1000)
             return
 
 
-    def destroy_enemy_defences(self, ct):
+    def bb_destroy_enemy_defences(self, ct): # NEEDS REWORK
         vision_tiles = ct.get_nearby_tiles()
         pos = ct.get_position()
 
@@ -298,12 +321,23 @@ class Player:
                     self.target = i
                     ct.draw_indicator_line(pos, i, 0, 0, 0)
 
-        self.go_to(ct)
+        self.bb_go_to(ct)
         if ct.can_build_gunner(self.target, self.target.direction_to(self.enemy_core_position)):
             ct.build_gunner(self.target, self.target.direction_to(self.enemy_core_position))
         if ct.get_position() == self.target:
             if ct.get_entity_type(ct.get_tile_building_id(ct.get_position())) == EntityType.BRIDGE:
                 ct.self_destruct()
+
+
+    def gn_attack_enemy_core(self, ct):
+        d = ct.get_direction()
+        target = ct.get_position()
+        for i in range(3):
+            target = target.add(d)
+            if ct.get_entity_type(ct.get_tile_builder_bot_id(target)) == EntityType.BUILDER_BOT and ct.get_team() == ct.get_team(ct.get_tile_building_id(target)):
+                return
+            if ct.can_fire(target):
+                ct.fire(target)
 
 
     def run(self, ct: Controller) -> None:
@@ -313,9 +347,9 @@ class Player:
             vision_tiles = ct.get_nearby_tiles()
             core_position_x, core_position_y = ct.get_position()[0], ct.get_position()[1]
             possible_core_locations = [
-                [ct.get_map_width() - core_position_x, core_position_y], # Horizontal Flip
-                [core_position_x, ct.get_map_height() - core_position_y], # Vertical Flip
-                [ct.get_map_width() - core_position_x, ct.get_map_height() - core_position_y]] # Rotation
+                [ct.get_map_width() - 1 - core_position_x, core_position_y], # Horizontal Flip
+                [core_position_x, ct.get_map_height() - 1- core_position_y], # Vertical Flip
+                [ct.get_map_width() - 1 - core_position_x, ct.get_map_height() - 1 - core_position_y]] # Rotation
 
             # First 3 bots have to find enemy base
             if self.num_spawned < 3:
@@ -369,7 +403,6 @@ class Player:
                             + self.enemy_core_position.y)
 
                     if ct.can_place_marker(i):
-                        ct.draw_indicator_line(ct.get_position(), self.enemy_core_position, 255, 0, 0)
                         ct.place_marker(i, message)
 
 
@@ -380,37 +413,35 @@ class Player:
                 return
             # Just spawned
             if self.status == 0:
-                self.initialise(ct)
+                self.bb_initialise(ct)
 
             # Find enemy core
             elif self.status == 1:
-                self.find_enemy_core(ct)
+                self.bb_find_enemy_core(ct)
 
             # Tell everyone about enemy core?
             elif self.status == 2:
-                self.spread_the_news_about_said_enemy_core(ct)
+                self.bb_spread_the_news_about_said_enemy_core(ct)
 
             #Look for ores
             elif self.status == 3:
-                self.find_ores(ct)
+                self.bb_find_ores(ct)
 
             elif self.status == 4:
-                self.build_conveyor_home(ct)
+                self.bb_build_conveyor_home(ct)
 
             elif self.status == 5:
-                self.go_to(ct, 4, True)
+                self.bb_go_to(ct, 4, True)
 
             elif self.status == 6:
-                self.go_to_enemy_core(ct)
+                self.bb_go_to_enemy_core(ct)
 
             elif self.status == 7:
-                self.destroy_enemy_defences(ct)
+                self.bb_destroy_enemy_defences(ct)
 
             else:
                 self.status = 3
 
 
         elif ct.get_entity_type() == EntityType.GUNNER:
-            d = ct.get_direction()
-            if ct.can_fire(ct.get_position().add(d)):
-                ct.fire(ct.get_position().add(d))
+            self.gn_attack_enemy_core(ct)
